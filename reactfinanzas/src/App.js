bc119e288afebcae3a57e35d2ef83b3301e63fec
RicardoPando <u201622564@upc.edu.pe>
2022-11-03 13:32:06 -0500

    React creado
    React creado listo para empezar

---
+++ reactfinanzas/src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter, Routes , Route ,Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
