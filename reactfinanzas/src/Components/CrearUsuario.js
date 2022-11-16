8dbd9701077618080bb33d575ad6cf50544a8dc6
AndresDoig0711 <andres.doig92@gmail.com>
2022-11-16 18:57:47 -0500

    LogIn and Register Design Improvements

--- reactfinanzas/src/Components/CrearUsuario.js
+++ reactfinanzas/src/Components/CrearUsuario.js
@@ -58,6 +58,39 @@ const CrearUsuario = () => {
                     </tbody>
                 </table>
             </form>
+            <div class="container">
+                <div class="screen">
+                    <div class="screen__content">
+                        <form class="login">
+                            <div class="login__field">
+                                <i class="login__icon fas fa-user"></i>
+                                <input type="text" class="login__input" placeholder="Nombre" name="nombre" onChange={handleChange}></input>
+                            </div>
+                            <div class="login__field">
+                                <i class="login__icon fas fa-lock"></i>
+                                <input type="password" class="login__input" placeholder="Contraseña" name="contrasegna" onChange={handleChange}></input>
+                            </div>
+                            <div class="login__field">
+                                <i class="login__icon fas fa-lock"></i>
+                                <input type="password" class="login__input" placeholder="Repetita Contraseña" nombre="contrasegna_" onChange={handleChange}></input>
+                            </div>
+                            <button class="button login__submit">
+                                
+                                <span class="button__text">Crear Usuario</span>
+                                
+                                <i class="button__icon fas fa-chevron-right"></i>
+                            </button>				
+                        </form>
+                        
+                    </div>
+                    <div class="screen__background">
+                        <span class="screen__background__shape screen__background__shape4"></span>
+                        <span class="screen__background__shape screen__background__shape3"></span>		
+                        <span class="screen__background__shape screen__background__shape2"></span>
+                        <span class="screen__background__shape screen__background__shape1"></span>
+                    </div>		
+                </div>
+            </div>
         </div>
     );
 }
--- reactfinanzas/src/Components/LogIn.js
+++ reactfinanzas/src/Components/LogIn.js
@@ -4,15 +4,52 @@ import './LogIn.css';
 const LogIn = () => {
     return (  
         <nav>
-          <ul>
+          {/* <ul>
             usuario:<input type={"text"}></input>
             <br />
             contraseña:<input type={"password"}></input>
             <br />
             <button class='button'>Iniciar Sesión</button>
             <Link to="usuario/crear">Registrarse</Link>
-          </ul>
-      </nav>
+          </ul> */}
+        
+        <div class="container">
+            <div class="screen">
+                <div class="screen__content">
+                    <form class="login">
+                        <div class="login__field">
+                            <i class="login__icon fas fa-user"></i>
+                            <input type="text" class="login__input" placeholder="Usuario"></input>
+                        </div>
+                        <div class="login__field">
+                            <i class="login__icon fas fa-lock"></i>
+                            <input type="password" class="login__input" placeholder="Contraseña"></input>
+                        </div>
+                        <button class="button login__submit">
+                            <Link to="usuario/listar">
+                            <span class="button__text">Iniciar Sesión</span>
+                            </Link>
+                            <i class="button__icon fas fa-chevron-right"></i>
+                        </button>	
+                        <button class="button register">
+                            <Link to="usuario/crear">
+                            <span class="button__text">Registrarse</span>
+                            </Link>
+                            <i class="button__icon fas fa-chevron-right"></i>
+                        </button>			
+                    </form>
+                    
+                </div>
+                <div class="screen__background">
+                    <span class="screen__background__shape screen__background__shape4"></span>
+                    <span class="screen__background__shape screen__background__shape3"></span>		
+                    <span class="screen__background__shape screen__background__shape2"></span>
+                    <span class="screen__background__shape screen__background__shape1"></span>
+                </div>		
+            </div>
+        </div>
+        </nav>
+        
     );
 }
  
